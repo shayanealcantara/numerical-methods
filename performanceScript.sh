bb4ad986164544d052e1ab29c5a67f63254056dd
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-04-30 23:07:44 -0300

    Adjust perfomanceScript

--- performanceScript.sh
+++ performanceScript.sh
@@ -1,14 +1,23 @@
 # Performance of Octave
-perf stat -r 10 -B -o results/octave50 octave T2.m 50 > /dev/null
-perf stat -r 10 -B -o results/octave400 octave T2.m 400 > /dev/null
-perf stat -r 2 -B -o results/octave1k octave T2.m 1000 > /dev/null
-# perf stat -r 1 -B -o results/octave5k octave T2.m 5000 > /dev/null
+perf stat -r 10 -B -o results/octave50 octave t2_octave.m 50 > /dev/null
+perf stat -r 10 -B -o results/octave400 octave t2_octave.m 400 > /dev/null
+perf stat -r 2 -B -o results/octave1000 octave t2_octave.m 1000 > /dev/null
+perf stat -r 1 -B -o results/octave5000 octave t2_octave.m 5000 > /dev/null
 
-# Performance of C
+# Performance of GSL C
 ulimit -s 100000000 # Increase Stack
-gcc T2.c -o t2.o -lgsl -lgslcblas
-perf stat -r 10 -B -o results/c50 ./t2.o 50 > /dev/null
-perf stat -r 10 -B -o results/c400 ./t2.o 400 > /dev/null
-perf stat -r 2 -B -o results/c1k ./t2.o 1000 > /dev/null
-# perf stat -r 1 -B -o results/c5k ./t2.o 5000 > /dev/null
+gcc t2_gsl.c -o t2_gsl.o -lgsl -lgslcblas
+perf stat -r 10 -B -o results/c50 ./t2_gsl.o 50 > /dev/null
+perf stat -r 10 -B -o results/c400 ./t2_gsl.o 400 > /dev/null
+perf stat -r 2 -B -o results/c1000 ./t2_gsl.o 1000 > /dev/null
+perf stat -r 1 -B -o results/c5000 ./t2_gsl.o 5000 > /dev/null
+ulimit -s 8192 # Set default Stack of my computer
+
+# Performance of LAPACK C
+gcc t2_lapack.c -o t2_lapack.o -llapacke
+ulimit -s 100000000 # Increase Stack
+perf stat -r 10 -B -o results/clapack50 ./t2_lapack.o 50 > /dev/null
+perf stat -r 10 -B -o results/clapack400 ./t2_lapack.o 400 > /dev/null
+perf stat -r 2 -B -o results/clapack1000 ./t2_lapack.o 1000 > /dev/null
+perf stat -r 1 -B -o results/clapack5000 ./t2_lapack.o 5000 > /dev/null
 ulimit -s 8192 # Set default Stack of my computer
